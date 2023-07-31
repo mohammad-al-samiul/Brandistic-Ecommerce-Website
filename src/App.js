41253af575e759bdc4c9a8f032fd08b11f661ad3
Noob-H4cker <alsamiul123@gmail.com>
2023-07-31 22:15:08 +0600

    layout complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes';
 
 const App = () => {
-  return <div>React Project Template</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
